e0af6bdd4782ba8df68cd4d61128d5695694e6b6
ksarpe <2299kasper@gmail.com>
2023-05-14 01:54:58 +0200

    in state of changing winning logic

--- js/spin.js
+++ js/spin.js
@@ -1,6 +1,9 @@
 let spinButton = document.getElementById('spin-button'); // Replace with your button's ID
 spinButton.addEventListener('click', function() {
-    if (spinning) {
+    if (spinning && spinsRemaining > 3 && spinsRemaining < 30) {
+            duration = 10;
+    }
+    else if (spinning){
         return;
     }
     else{
@@ -13,14 +16,15 @@ const reelNumber = 5; // Number of reels
 const rowNumber = 4; // Number of rows
 let spinsRemaining = 30; // Set the initial number of spins
 let spinning = false; // Set the initial spinning state
-const symbols = ['A', 'B', 'C', 'D', 'E']; // Define the symbols
+let duration = 60;
+const symbols = ['A', 'K', 'Q', 'J']; // Define the symbols , 'T', '9', '8'
 const reels = Array(rowNumber).fill('-'.repeat(reelNumber)); // Initial reel values
-
-
+let winningLines = [];
 
 function reset(){
     spinsRemaining = 30;
     spinning = false;
+    duration = 60;
 }
 
 
@@ -32,7 +36,7 @@ function spinReels() {
     anime({
         targets: '.reel',
         translateY: [0, 150], // Change this to adjust the distance of the animation
-        duration: 60, // Change this to adjust the speed of the animation
+        duration: duration, // Change this to adjust the speed of the animation
         easing: 'easeInOutQuad',
         complete: function() {
             const newSymbols = [];
@@ -65,9 +69,14 @@ function spinReels() {
             if (spinsRemaining > 0) {
                 setTimeout(spinReels, 0); // Adjust delay as needed
             } else {
+                if(reels.count == 4)
+                {reels.pop();}
+                
+                processWin();
+                
                 setTimeout(function() {
                     reset();
-                }, 300);
+                }, 500);
             }
         }
     });
@@ -81,19 +90,60 @@ function setReelColor(reelElement, symbol) {
         case 'A':
             reelElement.style.color = 'red';
             break;
-        case 'B':
-            reelElement.style.color = 'green';
-            break;
-        case 'C':
+        case 'K':
             reelElement.style.color = 'blue';
             break;
-        case 'D':
+        case 'Q':
+            reelElement.style.color = 'green';
+            break;
+        case 'J':
             reelElement.style.color = 'purple';
             break;
-        case 'E':
+        case 'T':
             reelElement.style.color = 'yellow';
+            break;
+        case '9':
+            reelElement.style.color = 'white';
+            break;
+        case '8':
+            reelElement.style.color = 'white';
             break;
         default:
             reelElement.style.color = 'white';
     }
+}
+
+function processWin(){
+    let winningLines = checkForWins();
+    let winAmount = calculateWin(winningLines);
+
+    console.log(`You won ${winAmount} points on lines ${winningLines.map(win => win.positions).join(', ')}`);
+}
+
+// This function checks for winning lines and returns an array of the winning line numbers and counts
+
+function checkForWins() {
+
+
+
+    for (let win of winningLines) {
+        for (let position of win.positions) {
+            const reelId = `reel${position[0] * reelNumber + position[1] + 1}`;
+            const reelElement = document.getElementById(reelId);
+            reelElement.classList.add('blink');
+        }
+    }
+    console.log(winningLines);
+    console.log(reels);
+    return winningLines;
+}
+
+// This function calculates the win amount based on the winning lines
+function calculateWin(winningLines) {
+    // For simplicity, let's say each winning line is worth 100 points times the count of same symbols
+    let totalWin = 0;
+    for (let win of winningLines) {
+        totalWin += win.count * 100;
+    }
+    return totalWin;
 }
